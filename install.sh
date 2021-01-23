09113f6230cc256f5f887ceaf887368e474f220a
bright2013 <bright2013@coupang.com>
2021-01-24 01:00:22 +0900

    Support linux as well

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 chmod +x ./bin/martin
-echo export MARTIN=$PWD >> ~/.bash_profile
-echo export PATH="$PATH:$PWD/bin" >> ~/.bash_profile
-exec ~/.bash_profile
-#kill -USR1 $PPID
+if [ -f ~/.bash_profile ]; then
+    echo export MARTIN=$PWD >> ~/.bash_profile
+    echo export PATH="$PATH:$PWD/bin" >> ~/.bash_profile
+    source ~/.bash_profile
+else 
+    echo export MARTIN=$PWD >> ~/.bashrc
+    echo export PATH="$PATH:$PWD/bin" >> ~/.bashrc
+    source ~/.bashrc
+fi
+kill -USR1 $PPID
 
