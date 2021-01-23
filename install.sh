853e9e17d23598c116b03296b34292ca75b6d974
jonghagi77 <jonghagi77@naver.com>
2021-01-24 00:48:18 +0900

    Edit install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 chmod +x ./bin/martin
 echo export MARTIN=$PWD >> ~/.bash_profile
 echo export PATH="$PATH:$PWD/bin" >> ~/.bash_profile
-exec ~/.bash_profile
-#kill -USR1 $PPID
+source ~/.bash_profile
+kill -USR1 $PPID
 
